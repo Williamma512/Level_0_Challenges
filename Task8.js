04cc79fb1d85c0fbd1ec9593f340f322ca76c200
William Makins <91621047+Williamma512@users.noreply.github.com>
2021-10-01 10:04:35 +0200

    Add files via upload

--- Task8.js
+++ Task8.js
@@ -1,7 +1,17 @@
+var pluralHour = "Hours";
+var singularHour = "Hour";
+var pluralminutes = "minutes";
+var singularminutes = "minute";
+var number;
 function convertingTime(number) {
     var Hour = Math.floor(number / 60);
     var minutes = number % 60;
-    return Hour + " Hour" + "," + minutes + " minutes";
-}
-console.log(convertingTime(71) );
+    if (number >120 || number >1) {
+        return (Hour + " " +  pluralHour + "," + minutes + " " + pluralminutes)
+    }
+    else
+    return (Hour + " " +  singularHour + "," + minutes + " " + singularminutes)
+    }
+
+console.log(convertingTime(1)) ;
 console.log(convertingTime(133));
