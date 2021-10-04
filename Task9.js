b3778af0a362d65c1732300bf6f43e97b6919534
William Makins <91621047+Williamma512@users.noreply.github.com>
2021-10-04 14:15:51 +0200

    Add files via upload

--- Task9.js
+++ Task9.js
@@ -1,14 +1,19 @@
-
-function findingvowels(userInput) {
-  var userInput = "Successful programmers know that it is really important to pay attention to detail"
+function findingvowels(userInput,userInputsecond) {
+  var userInput = "Umuzi"
+  var userInputsecond = "William"
   for (i = 0; i < userInput.length; i++)
   {
     if(userInput.charAt(i) == 'a' || userInput.charAt(i) == 'e' || userInput.charAt(i) == 'i' || userInput.charAt(i) == 'o'
     || userInput.charAt(i) == 'u')
     {
-     console.log("Vowels-" + userInput.charAt(i));
+     console.log("Umuzi Vowels-" + userInput.charAt(i));
     }
   }
-
+  for (i = 0; i < userInputsecond.length; i++)
+  if(userInput.charAt(i) == 'a' || userInput.charAt(i) == 'e' || userInput.charAt(i) == 'i' || userInput.charAt(i) == 'o'
+    || userInput.charAt(i) == 'u')
+    {
+     console.log("William Vowels-" + userInputsecond.charAt(i));
+    }
 }
 findingvowels();
