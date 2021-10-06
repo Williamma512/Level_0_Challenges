b52e8dcf4c082562551779ff875463422a3ce3a5
William Makins <91621047+Williamma512@users.noreply.github.com>
2021-10-06 08:37:50 +0200

    Add files via upload

--- Tast6.js
+++ Tast6.js
@@ -1,10 +1,14 @@
 var number1 = 50;
 var number2 = 5;
 var number3 = 6;
-if (number2 >= number1 && >= number3)
-    console.log( number2 + "is the biggest number.");
-    else if (number1 >= && number2 >= number3);
-    Console.log( number1 + " is the biggest number.");
-    else
-    Console.log(number3 + "is the biggest number.");
+function biggerstNumber() {
+    if (number2 >= number1 || number2 >= number3)
+        console.log( number2 + "is the biggest number.");
+        else if (number1 >= number2 || number1 >= number3)
+        console.log( number1 + " is the biggest number.")
+        else
+        console.log(number3 + "is the biggest number.");
+    }
+biggerstNumber();
+    
     
