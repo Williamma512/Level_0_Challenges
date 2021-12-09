bf119e89a001ff3f18633781252644ed54dafb68
William Makins <91621047+Williamma512@users.noreply.github.com>
2021-12-09 14:26:42 +0200

    Delete Task5.js

--- Task5.js
+++
@@ -1,8 +0,0 @@
-function triangleArea(side1,side2,side3) {
-    var p = (side1 + side2 + side3)/2;
-    var a = Math.sqrt(p * (p - side1)* (p - side2) * (p - side3 ));
-
-console.log ("The area of the triangle is :" + a + " Square cm");
-}
-
-triangleArea(3,4,5);
