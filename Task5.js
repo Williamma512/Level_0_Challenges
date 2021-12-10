1194b4dc0d387d26fca39ff7c67720c08d01b8a9
William Makins <91621047+Williamma512@users.noreply.github.com>
2021-12-10 21:00:24 +0200

    Updated required information.

--- Task5.js
+++ Task5.js
@@ -1,6 +1,6 @@
 function triangleArea(side1,side2,side3) {
-    var p = (side1 + side2 + side3)/2;
-    var a = Math.sqrt(p * (p - side1)* (p - side2) * (p - side3 ));
+    const p = (side1 + side2 + side3)/2;
+    const a = Math.sqrt(p * (p - side1)* (p - side2) * (p - side3 ));
 
 console.log ("The area of the triangle is :" + a + " Square cm");
 }
--- Task7.js
+++ Task7.js
@@ -1,10 +1,10 @@
 function convercelsiustoF(celsius) {
-    var CelsiusToFehrenheit = celsius * 9 / 5 + 32;
+    const CelsiusToFehrenheit = celsius * 9 / 5 + 32;
     return(celsius + ' \xB0C is ' + CelsiusToFehrenheit + ' \xB0F.');
 }
 
 function convertfehrenheitToC(fehrenheit) {
-    var fehrenheitToCelcius = (fehrenheit -32) * 5 / 9;
+    const fehrenheitToCelcius = (fehrenheit -32) * 5 / 9;
     return(fehrenheit + ' \xB0F is ' + fehrenheitToCelcius + ' \xB0C.');
 }
 convercelsiustoF(35);
